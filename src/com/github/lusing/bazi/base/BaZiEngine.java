ac78cdac3f8b0443f60af113ba1195fbdd4c75ca
Louis <lusing@gmail.com>
2013-07-06 16:20:35 +0800

    Add p3 and p4 method

--- src/com/github/lusing/bazi/base/BaZiEngine.java
+++ src/com/github/lusing/bazi/base/BaZiEngine.java
@@ -1,17 +1,19 @@
 package com.github.lusing.bazi.base;
 
 /**
- * 天干代表所见到的表面，外性格。性格、行为举止、容颜、六亲状况、有钱、凶灾。<p/> 地支代表内心，内环境。善恶、忠奸、心中想法。
+ * 天干代表所见到的表面，外性格。性格、行为举止、容颜、六亲状况、有钱、凶灾。
+ * <p/>
+ * 地支代表内心，内环境。善恶、忠奸、心中想法。
  * 
  * @author Louis
  */
 
 public class BaZiEngine {
-    public static final int YEAR = 0;
-    public static final int MONTH = 1;
-    public static final int DAY = 2;
-    public static final int HOUR = 3;
-    
+	public static final int YEAR = 0;
+	public static final int MONTH = 1;
+	public static final int DAY = 2;
+	public static final int HOUR = 3;
+
 	int year_tg;
 	int year_dz;
 	int month_tg;
@@ -36,7 +38,7 @@ public class BaZiEngine {
 
 		tgs = new TianGan[4];
 		dzs = new DiZhi[4];
-                
+
 		tgs[0] = new TianGan(y1);
 		tgs[1] = new TianGan(m1);
 		tgs[2] = new TianGan(d1);
@@ -48,60 +50,94 @@ public class BaZiEngine {
 		dzs[3] = new DiZhi(h2);
 
 	}
-        
-        public void run(){
-            checkLiuQin();
-            checkWuXing();
-        }
-        
-        private void checkLiuQin(){
-            System.out.println("日主："+tgs[2]);
-            System.out.println(tgs[0]+" "+ShiShen.getShiShen(tgs[2], tgs[0]).toString());
-            System.out.println(tgs[1]+" "+ShiShen.getShiShen(tgs[2], tgs[1]).toString());
-            System.out.println(tgs[3]+" "+ShiShen.getShiShen(tgs[2], tgs[3]).toString());
-        }
-        
-        private void checkWuXing(){
-            for(int i=0;i<tgs.length;i++){
-            System.out.print(tgs[i].getXing().toString()+dzs[i].getXing()+"\t");
-            }
-            System.out.println();
-            
-            System.out.println(checkWangShuai());
-        }
-        
-        /**
-         * 检查日主的旺衰
-         */
-        private BaziResult checkWangShuai(){
-            return checkGenYinXiangFu();
-        }
-        
-        /**
-         * 根印帮扶法
-         */
-        private BaziResult checkGenYinXiangFu(){
-            boolean isWang = false;
-            //1.2.日主在月令得根或印一次便是有旺的可能，再得年支或日支根一次便以旺论, 不必再看天干。
-            int gen = tgs[DAY].getXing().getXing();
-            if(isGen(dzs[MONTH]) || isYin(dzs[MONTH])){
-                if(isGen(dzs[YEAR]) || isGen(dzs[DAY]))
-                    return new BaziResult(BaziResult.WANG,"图1~2");
-            }
-            
-            //3.日主根印临日时支以身弱论命。
-            //4. 如果天干全是印比则以身旺论。
-            
-            //5. 印星临年月以弱论。即使天干印比一片也弱。地支中印星同时出现两个，不论在何位置均以弱论。
-            
-            return new BaziResult(BaziResult.UNKNOWN,"暂时还处理不了");
-        }
-        
-        private boolean isGen(DiZhi dz){
-            return dz.getXing().getXing() == tgs[DAY].getXing().getXing();
-        }
-        
-        private boolean isYin(DiZhi dz){
-            return dz.getXing().isSheng(tgs[DAY].getXing());
-        }
+
+	public void run() {
+		checkLiuQin();
+		checkWuXing();
+	}
+
+	private void checkLiuQin() {
+		System.out.println("日主：" + tgs[2]);
+		System.out.println(tgs[0] + " "
+				+ ShiShen.getShiShen(tgs[2], tgs[0]).toString());
+		System.out.println(tgs[1] + " "
+				+ ShiShen.getShiShen(tgs[2], tgs[1]).toString());
+		System.out.println(tgs[3] + " "
+				+ ShiShen.getShiShen(tgs[2], tgs[3]).toString());
+	}
+
+	private void checkWuXing() {
+		for (int i = 0; i < tgs.length; i++) {
+			System.out.print(tgs[i].getXing().toString() + dzs[i].getXing()
+					+ "\t");
+		}
+		System.out.println();
+
+		System.out.println(checkWangShuai());
+	}
+
+	/**
+	 * 检查日主的旺衰
+	 */
+	private BaziResult checkWangShuai() {
+		return checkGenYinXiangFu();
+	}
+
+	/**
+	 * 根印帮扶法
+	 */
+	private BaziResult checkGenYinXiangFu() {
+		boolean isWang = false;
+		// 1.2.日主在月令得根或印一次便是有旺的可能，再得年支或日支根一次便以旺论, 不必再看天干。
+		int gen = tgs[DAY].getXing().getXing();
+		if (isGen(dzs[MONTH]) || isYin(dzs[MONTH])) {
+			if (isGen(dzs[YEAR]) || isGen(dzs[DAY]))
+				return new BaziResult(BaziResult.WANG, "图1~2");
+		}
+
+		// 3.日主根印临日时支以身弱论命。
+		if (isGen(dzs[DAY])) {
+			if(isGenYin(dzs[HOUR])){
+				if(isAllBiOrYin()){
+					return new BaziResult(BaziResult.WANG, "图4");
+				}
+				else{
+					return new BaziResult(BaziResult.RUO, "图3");
+				}
+			}
+		}
+		// 4. 如果天干全是印比则以身旺论。
+
+		// 5. 印星临年月以弱论。即使天干印比一片也弱。地支中印星同时出现两个，不论在何位置均以弱论。
+
+		return new BaziResult(BaziResult.UNKNOWN, "暂时还处理不了");
+	}
+
+	private boolean isGen(DiZhi dz) {
+		return dz.getXing().getXing() == tgs[DAY].getXing().getXing();
+	}
+
+	private boolean isYin(DiZhi dz) {
+		return dz.getXing().isSheng(tgs[DAY].getXing());
+	}
+	
+	private boolean isGenYin(DiZhi dz){
+		return isGen(dz) || isYin(dz);
+	}
+	
+	private boolean isBi(TianGan tg){
+		return tgs[DAY].getXing().getXing() == tg.getXing().getXing();
+	}
+	
+	private boolean isYin(TianGan tg){
+		return tg.isSheng(tgs[DAY]);
+	}
+	
+	private boolean isAllBiOrYin(){
+		boolean result = true;
+		for(int i=YEAR;i<=HOUR;i++){
+			result = result && (isYin(tgs[i]) || isBi(tgs[i]));
+		}
+		return result;
+	}
 }
--- src/com/github/lusing/bazi/base/Main.java
+++ src/com/github/lusing/bazi/base/Main.java
@@ -17,12 +17,22 @@ public class Main {
 		BaZiEngine bze2 = new BaZiEngine(TianGan.JI, DiZhi.HAI, TianGan.GUI,
 				DiZhi.YOU, TianGan.GUI, DiZhi.MAO, TianGan.REN, DiZhi.XU);
 		bze2.run();
-		
-		//根印帮扶法
-                //例1, 
+
+		// 根印帮扶法
+		// 例1,
 		BaZiEngine bze3 = new BaZiEngine(TianGan.GUI, DiZhi.MAO, TianGan.BING,
 				DiZhi.CHEN, TianGan.JI, DiZhi.CHOU, TianGan.YI, DiZhi.HAI);
 		bze3.run();
-                
+
+		// 图3
+		BaZiEngine bze_3 = new BaZiEngine(TianGan.JIA, DiZhi.WU, TianGan.JIA,
+				DiZhi.XU, TianGan.GUI, DiZhi.HAI, TianGan.XIN, DiZhi.YOU);
+		bze_3.run();
+
+		BaZiEngine bze_3_2 = new BaZiEngine(TianGan.GENG, DiZhi.WU,
+				TianGan.REN, DiZhi.XU, TianGan.GUI, DiZhi.HAI, TianGan.XIN,
+				DiZhi.YOU);
+		bze_3_2.run();
+
 	}
 }
